debf34709efd43e2f90d84c6f4e60df56a820ddd
stephy cyriac <cyriacstephy@gmail.com>
2021-09-14 17:54:53 +0530

    edit button done

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const generateNewCard = (taskData) =>
 `  <div class="col-md-6 col-lg-4" >
 <div class="card text-center">
     <div class="card-header d-flex justify-content-end gap-2">
-        <button type="button" class="btn btn-outline-success"><i class="fas fa-pen"></i></button>
+        <button type="button" class="btn btn-outline-success" id=${taskData.id} onclick="editCard.apply(this,arguments)">
+        <i class="fas fa-pen" id=${taskData.id} onclick="editCard.apply(this,arguments)"> </i>
+        </button>
         <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)" >
         <i class="fas fa-trash" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i>
         </button>
@@ -81,6 +83,34 @@ else{
 
 
 
+};
+
+const editCard = (event) => {
+    event  = window.event;
+    const targetID = event.target.id;
+    const tagname = event.target.tagName; 
+
+    let parentElement;
+    if (tagname ==="BUTTON"){
+        parentElement=event.target.parentNode.parentNode;
+    }
+    else{
+        parentElement=event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    
+
+    //submit button
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+
+    submitButton.innerHTML="Save Changes"
 };
 //issues
 //closing of modal (using bootsrap)                      --> {solved}
