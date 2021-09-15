2839874f4bc87534bd0979afdb590acf71dd83d2
stephy cyriac <cyriacstephy@gmail.com>
2021-09-15 09:11:42 +0530

    editing done

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const generateNewCard = (taskData) =>
       <a href="#" class="btn btn-primary">${taskData.taskType}</a>
     </div>
     <div class="card-footer">
-        <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+        <button type="button" id=${taskData.id} class="btn btn-outline-primary float-end">Open Task</button>
     </div>
   </div>
 </div>
@@ -109,11 +109,56 @@ const editCard = (event) => {
     taskTitle.setAttribute("contenteditable", "true");
     taskDescription.setAttribute("contenteditable", "true");
     taskType.setAttribute("contenteditable", "true");
-
-    submitButton.innerHTML="Save Changes"
+    submitButton.setAttribute("onclick","saveEditchanges.apply(this,arguments)");
+    submitButton.innerHTML="Save Changes";
 };
+
+const saveEditChanges=(event) =>{
+    event  = window.event;
+    const targetID = event.target.id;
+    const tagname = event.target.tagName; 
+
+    let parentElement;
+    if (tagname ==="BUTTON"){
+        parentElement=event.target.parentNode.parentNode;
+    }
+    else{
+        parentElement=event.target.parentNode.parentNode.parentNode;
+    }
+
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+
+const updatedData = {
+    taskTitle: taskTitle.innerHTML,
+    taskType:taskType.innerHTML,
+    taskDescription:taskDescription.innerHTML,
+};
+globalStore = globalStore.map((task)=>{
+    if (task.id=== targetID){
+return{
+    id:task.id, //unique number for id
+    imageUrl:task.imageUrl,
+    taskTitle: updatedData.taskTitle,
+    taskType:updatedData.taskType,
+    taskDescription:updatedData.taskDescription,
+};
+    }
+    return task;   //important
+});
+updateLocalStorage();
+taskTitle.setAttribute("contenteditable", "false");
+    taskDescription.setAttribute("contenteditable", "false");
+    taskType.setAttribute("contenteditable", "false");
+   submitButton.removeAttribute("onclick");
+    submitButton.innerHTML="Open Task";
+};
+
+
 //issues
 //closing of modal (using bootsrap)                      --> {solved}
 //adding data to local storage                            --> {solved}
 //deleting a card (using id)                              --> {solved}
-//editing a card
+//editing a card                                        --> {solved}
+//saving edited card
